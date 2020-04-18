e1134bcdf27e4e534016b73ad0918000c676c9ca
VolodymyrMazurets <volodymyrmazurets@gmail.com>
2020-04-18 14:51:48 +0300

    :sparkles: added sections; adaptive;

--- projects/mogo/src/js/app.js
+++ projects/mogo/src/js/app.js
@@ -1,7 +1,13 @@
 const navBar = document.getElementById("navBar");
+const burger = document.getElementById("burger");
+const mobileMenu = document.getElementById("mobileNav");
 
 document.addEventListener("scroll", () =>
   window.pageYOffset < navBar.clientHeight
     ? navBar.classList.remove("filled")
     : navBar.classList.add("filled")
 );
+
+burger.addEventListener("click", () => {
+  mobileMenu.classList.toggle('active');
+});
